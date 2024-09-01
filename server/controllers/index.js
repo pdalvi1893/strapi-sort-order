b00d49dee31de747a208b94c361fbb804143bdb5
unknown <pankaj@tentwenty.me>
2024-09-01 16:46:38 +0530

    fix: removed unnecessary files

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const myController = require("./my-controller");
 const sortOrderController = require("./sort-order");
 
 module.exports = {
-  myController,
   sortOrderController,
 };
--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,7 @@
 "use strict";
 
-const myService = require("./my-service");
 const sortOrder = require("./sort-order");
 
 module.exports = {
-  myService,
   sortOrder,
 };
